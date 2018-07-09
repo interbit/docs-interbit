c75238803404875d472f0881c29650b3c87f1efb
Ewan Edwards <eskwayrd@BTL-EE.local>
2018-07-09 16:10:01 -0700

    Remove bundler installation; now installed using multipl Heroku buildpacks.

--- make.sh
+++ make.sh
@@ -2,10 +2,6 @@
 
 echo 'Building documentation, please wait... '
 
-# Install Asciidoctor and dependencies
-sudo gem install bundler
-bundle install
-
 # Install and build gitbook
 gitbook install
 gitbook build
