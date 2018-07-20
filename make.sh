4998346d013e5da7ee4f4498732d0fe563abfba2
Ewan Edwards <ewan@blockchaintechltd.com>
2018-07-20 15:53:00 -0700

    Add post-build cleanup of the _book folder.

--- make.sh
+++ make.sh
@@ -5,3 +5,10 @@ echo 'Building documentation, please wait... '
 # Install and build gitbook
 gitbook install
 gitbook build
+
+# Post-build cleanup
+cd _book
+rm Gemfile Gemfile.lock Makefile make.sh Rakefile
+rm -f npm-debug.log package.json package-lock.json
+rm index.js local.js app.json README.md .gitignore
+rm -rf vendor
