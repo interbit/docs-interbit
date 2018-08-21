280be060284048a4a281e7da841eca06a73874b8
Ewan Edwards <ewan@blockchaintechltd.com>
2018-08-21 13:22:59 -0700

    Add step to clone Interbit repo for source comment parsing.

--- make.sh
+++ make.sh
@@ -5,6 +5,10 @@ echo 'Building documentation, please wait... '
 # Make sure Ruby dependencies installed
 bundle install || echo "Ruby dependencies failed to install; make sure Bundler is installed!"
 
+# Clone the Interbit repo, so we can collect source comments.
+rm -rf _interbit
+git clone https://github.com/interbit/interbit.git _interbit
+
 # Install and build gitbook
 gitbook install
 gitbook build
