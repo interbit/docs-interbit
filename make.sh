93b9af90540e8dd328d48c237a562cb3abebd817
Ewan Edwards <ewan@blockchaintechltd.com>
2018-08-27 16:21:39 -0700

    Add processing to auto-generate API markup via jsdoc:
    * Add a jsdoc configuration file.
    * Runs jsdoc on specific packages, capturing API details from source
      comments.
    * Converts the JSON from jsdoc into Asciidoc markup.
    * Adds reference documentation that includes the generated Asciidoc
      markup.
    * Adds NPM commands to package.json that allow the spell and includes CI
      checks to be executed explicitly.
    * make.sh now uses console colors to delineate processing stages.

--- make.sh
+++ make.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
-echo 'Building documentation, please wait... '
+echo -e "\033[34mBuilding documentation, please wait...\033[0m"
 
 # Make sure Ruby dependencies installed
-bundle install || echo "Ruby dependencies failed to install; make sure Bundler is installed!"
-
-# Clone the Interbit repo, so we can collect source comments.
-rm -rf _interbit
-git clone https://github.com/interbit/interbit.git _interbit
+bundle install || echo -e "\033[31mRuby dependencies failed to install; make sure Bundler is installed!\033[0m"
 
 # Make sure that `gitbook` command is installed
 if ! [ -x "$(command -v gitbook)" ]; then
-  echo "Installing GitBook (and other node dependencies)..."
+  echo -e "\033[34mInstalling GitBook (and other node dependencies)...\033[0m"
   npm i
 fi
 
 # Install GitBook plugins, themes, etc.
-echo "Installing GitBook plugins, themes, etc..."
+echo -e "\033[34mInstalling GitBook plugins, themes, etc...\033[0m"
 gitbook install
 
+# Capture source comments for select packages
+echo -e "\033[34mCollecting API details...\033[0m"
+
+rm -rf _interbit
+git clone https://github.com/interbit/interbit.git _interbit
+./api.sh
+
 # Build the book's HTML
-echo "Building the documentation..."
+echo -e "\033[34mBuilding the documentation...\033[0m"
 gitbook build
 
 # Post-build cleanup
 if [ -d "_book" ]; then
-  echo "Performing post-build cleanup..."
+  echo -e "\033[34mPerforming post-build cleanup...\033[0m"
   cd _book
   rm Gemfile Gemfile.lock Makefile make.sh Rakefile
-  rm -f npm-debug.log package.json package-lock.json
+  rm -f npm-debug.log package.json package-lock.json conf.jsdoc
   rm index.js local.js app.json README.md .gitignore
-  rm -rf vendor
+  rm -rf vendor _interbit apiadoc
 else
-  echo "*** Looks like the doc build failed!"
+  echo -e "\033[1m\033[31m*** Looks like the doc build failed!\033[0m"
 fi
 
-echo "Done!"
+echo -e "\033[1m\033[34mDone!\033[0m"
