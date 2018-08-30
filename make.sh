de930ccc4aff4cce13fea6a3f2ec0addde0ee2c6
Ewan Edwards <ewan@blockchaintechltd.com>
2018-08-30 16:08:29 -0700

    Move the post-build cleanup steps into a separate script, so that they can be called independently.

--- make.sh
+++ make.sh
@@ -27,15 +27,6 @@ echo -e "\033[34mBuilding the documentation...\033[0m"
 gitbook build
 
 # Post-build cleanup
-if [ -d "_book" ]; then
-  echo -e "\033[34mPerforming post-build cleanup...\033[0m"
-  cd _book
-  rm Gemfile Gemfile.lock Makefile make.sh Rakefile
-  rm -f npm-debug.log package.json package-lock.json conf.jsdoc
-  rm index.js local.js app.json README.md .gitignore
-  rm -rf vendor _interbit apiadoc
-else
-  echo -e "\033[1m\033[31m*** Looks like the doc build failed!\033[0m"
-fi
+./cleanup.sh
 
 echo -e "\033[1m\033[34mDone!\033[0m"
